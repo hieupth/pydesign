2eeca95e85a9bca44d3283b74cfee56d6bef9fd3
Hieu Tr. Pham <hieupt.ai@gmail.com>
2020-08-18 15:48:07 +0700

    Update package version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open("README.md", "r") as fh:
 setup(
     name='pyarchitect',
     packages=find_packages(),
-    version='0.0.4',
+    version='0.0.5',
     license='MIT',
     description='Implementations of common data structures, design patterns and useful utilities',
     long_description=long_description,
