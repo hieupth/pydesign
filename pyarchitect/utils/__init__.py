081aa1382d513f68600f36be1e4b5a1e757d7f70
Hieu Tr. Pham <hieupt.ai@gmail.com>
2020-08-24 16:57:27 +0700

    Update dict utils

---
+++ pyarchitect/utils/__init__.py
@@ -0,0 +1,22 @@
+# ------------------------------------------------------------------------------
+#  GNU General Public License
+#
+#  Copyright (c) 2020, Hieu Pham.
+#
+#  This file is part of Py-Architect.
+#  <https://github.com/hieupth/pyarchitect>
+#
+#  Py-Architect is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Py-Architect is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Py-Architect. If not, see <https://www.gnu.org/licenses/>.
+# ------------------------------------------------------------------------------
+from __future__ import absolute_import
